43db98da035141b5be8dd62ae735e5c082141149
liuhuaye <spring_coming@foxmail.com>
2025-04-16 20:51:07 +0800

    添加用户和地址类，更新 HelloController，增加新的请求映射和响应内容，更新 README 以包含 HTTP 状态码说明，新增 VSCode 配置文件

--- WebDay1/demo1/src/main/java/example/demo1/HelloController.java
+++ WebDay1/demo1/src/main/java/example/demo1/HelloController.java
@@ -1,15 +1,30 @@
 package example.demo1;
-import org.springframework.web.bind.annotation.GetMapping;
+import example.demo1.pojo.User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
-    @GetMapping("/aaa")
-    public String hello(){
-      System.out.println("hello world");
-      return "hello world";
-    }
+  @RequestMapping("/simple")
+  public String simpleParam(String name, Integer age) {
+    System.out.println("name:" + name + ",age:" + age);
+    return "ok";
+  }
+  
+  @RequestMapping("/hello")
+  public String helloWorld() {
+    return "你好，世界！欢迎学习 JavaWeb 后端开发！";
+  }
 
+  @RequestMapping("/pojo")
+  public String pojoParam(User user) {
+    System.out.println("user:" + user);
+    return "ok";
+  }
+
+  @RequestMapping("/pojo2")
+  public String pojoParam2(User user) {
+    System.out.println("user:" + user);
+    return "ok";
+  }
 }
- 
